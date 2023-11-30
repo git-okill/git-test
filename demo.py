0bb17f5c7d3c9aa45cb7785d23e62e7b84a64be7
git-okill <151806805+git-okill@users.noreply.github.com>
2023-11-30 11:00:29 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,3 +3,7 @@ consectetur adipiscing elit,
 sed do eiusmod tempoar incididunt
 ut labore et dolore magna asdddddddddddddddddddddddaliquaasdasdasdasds.'''
 print(a)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
